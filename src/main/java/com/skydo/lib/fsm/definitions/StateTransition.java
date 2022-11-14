aa24c9b39b5fa90b99dc2ee3f2fec5e5f8617136
Raj Sheth <rajpsheth@gmail.com>
2022-11-14 13:08:11 +0530

    Add code comments

--- src/main/java/com/skydo/lib/fsm/definitions/StateTransition.java
+++ src/main/java/com/skydo/lib/fsm/definitions/StateTransition.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Used over a class where all the transition validation functions are declared.
+ * Methods declared inside the consumer class will have `TransitionValidator` annotation
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface StateTransition {
--- src/main/java/com/skydo/lib/fsm/definitions/TransitionValidator.java
+++ src/main/java/com/skydo/lib/fsm/definitions/TransitionValidator.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Declared over a validation function
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface TransitionValidator {
