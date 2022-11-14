08da9d004d7fa89d18d9c7ceb62365fc5806dc65
Raj Sheth <rajpsheth@gmail.com>
2022-11-14 19:10:56 +0530

    Rename `entityFieldValidatorMap`

--- src/main/java/com/skydo/lib/fsm/config/StateValidatorConfig.java
+++ src/main/java/com/skydo/lib/fsm/config/StateValidatorConfig.java
@@ -17,7 +17,7 @@ public class StateValidatorConfig {
 
     private final Logger log = LoggerFactory.getLogger(StateValidatorConfig.class.getSimpleName());
 
-    private final HashMap<Class<?>, HashMap<String, HashMap<String, Method>>> entityToFieldMap = new HashMap<>();
+    private final HashMap<Class<?>, HashMap<String, HashMap<String, Method>>> entityFieldValidatorMap = new HashMap<>();
 
     /**
      * e.g. Exporter
@@ -33,78 +33,38 @@ public class StateValidatorConfig {
         }
      */
     public HashMap<Class<?>, HashMap<String, HashMap<String, Method>>> getValidatorMap() {
-        return entityToFieldMap;
+        return entityFieldValidatorMap;
     }
 
     public void createValidatorMap() {
-
         Set<String> stateMachinePackageScanConfig = EnableStateMachinePackages.Registrar.stateMachinePackageScanConfig;
-
         log.info(stateMachinePackageScanConfig.toString());
-
         log.info(String.join(",", stateMachinePackageScanConfig));
-
         Reflections reflections = new Reflections(new ConfigurationBuilder()
                 .forPackages(StringUtils.toStringArray(stateMachinePackageScanConfig))
                 .setScanners(Scanners.MethodsAnnotated, Scanners.TypesAnnotated));
 
         Set<Class<?>> validatorClasses =
                 reflections.get(Scanners.TypesAnnotated.with(StateTransition.class).asClass());
-
         for (Class<?> validatorClass : validatorClasses) {
-
             StateTransition stateMachineAnnotation = validatorClass.getAnnotation(StateTransition.class);
-
             Class<?> entity = stateMachineAnnotation.entity();
-
             String field = stateMachineAnnotation.field();
-
-            if (!entityToFieldMap.containsKey(entity)) {
-                entityToFieldMap.put(entity, new HashMap<>());
+            if (!entityFieldValidatorMap.containsKey(entity)) {
+                entityFieldValidatorMap.put(entity, new HashMap<>());
             }
 
-            HashMap<String, HashMap<String, Method>> fieldToValuesMap = entityToFieldMap.get(entity);
-
+            HashMap<String, HashMap<String, Method>> fieldToValuesMap = entityFieldValidatorMap.get(entity);
             if (!fieldToValuesMap.containsKey(field)) {
                 fieldToValuesMap.put(field, new HashMap<>());
             }
-
             HashMap<String, Method> valuesToValidators = fieldToValuesMap.get(field);
-
-
             Method[] validators = validatorClass.getDeclaredMethods();
-
             for (Method validator : validators) {
-
                 TransitionValidator validatorAnnotation = validator.getAnnotation(TransitionValidator.class);
-
                 String fieldValue = validatorAnnotation.state();
-
                 valuesToValidators.put(fieldValue, validator);
-
             }
-
-
         }
-
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
