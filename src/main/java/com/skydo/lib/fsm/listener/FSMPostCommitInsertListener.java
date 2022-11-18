6c3b4fc0683ef4f3badf29df7ab8620abba1849e
Raj Sheth <rajpsheth@gmail.com>
2022-11-18 17:30:36 +0530

    Add FSMPostCommitIntegrator

---
+++ src/main/java/com/skydo/lib/fsm/listener/FSMPostCommitInsertListener.java
@@ -0,0 +1,104 @@
+package com.skydo.lib.fsm.listener;
+
+import com.skydo.lib.fsm.config.StateValidatorConfig;
+import com.skydo.lib.fsm.servicecontributor.FSMService;
+import org.hibernate.event.spi.PostCommitInsertEventListener;
+import org.hibernate.event.spi.PostInsertEvent;
+import org.hibernate.persister.entity.EntityPersister;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Raj Sheth
+ */
+
+public class FSMPostCommitInsertListener extends BaseEventListener implements PostCommitInsertEventListener {
+
+
+	private final static Logger log = LoggerFactory.getLogger(FSMPostCommitUpdateListener.class);
+
+	public FSMPostCommitInsertListener(FSMService fsmService) {
+		super(fsmService);
+	}
+
+	private boolean onPostCommit(PostInsertEvent postInsertEvent) {
+
+		StateValidatorConfig stateValidatorConfig = getFsmService().getStateValidator();
+		HashMap<Class<?>, HashMap<String, HashMap<String, Method>>> entityFieldPostUpdateActionMap
+				= stateValidatorConfig.getEntityFieldPostUpdateActionMap();
+
+		Object entity = postInsertEvent.getEntity();
+
+		Class<? extends Object> entityClass = entity.getClass();
+		HashMap<String, HashMap<String, Method>> fieldToValuesMap = entityFieldPostUpdateActionMap.get(entityClass);
+
+		if (entityFieldPostUpdateActionMap.containsKey(entityClass)) {
+			String[] propertyNames = postInsertEvent.getPersister().getPropertyNames();
+			List newValues = Arrays.stream(postInsertEvent.getState()).toList();
+			int totalFields = newValues.size();
+
+			for(int i = 0 ; i < totalFields ; ++i) {
+				String propertyName = propertyNames[i];
+				Object currentNewValue = newValues.get(i).toString();
+
+				if (fieldToValuesMap.containsKey(propertyName)) {
+					HashMap<String, Method> valuesToPostActionMethods = fieldToValuesMap.get(propertyName);
+
+					if (valuesToPostActionMethods.containsKey(currentNewValue)) {
+						log.info("Yes:: `valuesToPostActionMethods` contains the currentNewValue: " + currentNewValue);
+						Method postActionMethod = valuesToPostActionMethods.get(currentNewValue);
+						Class<?> declaringClass = postActionMethod.getDeclaringClass();
+						try {
+							postActionMethod.invoke(
+								// TODO: Is this assumption correct? Accessing constructor at zeroth index?
+								declaringClass.getConstructors()[0].newInstance(),
+								postInsertEvent.getId(),
+								// TODO: can we handle this better?
+								//  Old value is empty string
+								"",
+								currentNewValue
+							);
+						} catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+
+	/**
+	 * Called when a commit fails and an entity was scheduled for insertion
+	 *
+	 * @param event the insert event to be handled
+	 */
+	@Override
+	public void onPostInsertCommitFailed(PostInsertEvent event) {
+
+	}
+
+	@Override
+	public void onPostInsert(PostInsertEvent event) {
+		onPostCommit(event);
+	}
+
+	/**
+	 * Does this listener require that after transaction hooks be registered?
+	 *
+	 * @param persister The persister for the entity in question.
+	 * @return {@code true} if after transaction callbacks should be added.
+	 * @deprecated use {@link #requiresPostCommitHandling(EntityPersister)}
+	 */
+	@Override
+	public boolean requiresPostCommitHanding(EntityPersister persister) {
+		return true;
+	}
+}
